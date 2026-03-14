f243cd455996fcc5ff27444bb4453e5624c83e64
Gaby Gomes <gabriella.gomes_ar@hotmail.com>
2026-03-14 17:34:39 -0300

    Iniciando Header

---
+++ src/app/portfolio/components/knowledge/knowledge.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-knowledge',
+  imports: [],
+  templateUrl: './knowledge.html',
+  styleUrl: './knowledge.css',
+})
+export class Knowledge {}
